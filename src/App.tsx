0c4bd0a8590d9726fa3adf025a95e58050658299
LeandroRMachado <leandroskullofwar@outlook.com>
2022-02-26 22:46:58 -0300

    finish front-end, create components and your style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { useContext } from "react"
 import styles from "./App.module.scss"
 import { LoginBox } from "./components/LoginBox"
 import { MessageList } from "./components/MessageList"
+import { SendMessageForm } from "./components/SendMessageForm"
+import { AuthContext } from "./contexts/auth"
 
 export function App() {
+  const { user } = useContext(AuthContext)
 
   return (
-    <main className={styles.contentWrapper}>
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
       <MessageList />
-      <LoginBox />
+      { !!user ? <SendMessageForm /> : <LoginBox /> }
     </main>
   )
 }
+
+// quando eu utilizo o "!!", eu espero receber um boolean(true,false) - nesse caso "se o meu user não estiver preenchido eu apresendo o meu SendMessageForm, se não LoginBox"
